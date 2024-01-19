4e55e7ed1895a2727d334277401d7392484d7fbc
momermughal <m.omer@remitchoice.email>
2024-01-19 12:48:44 +0500

    merge

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,10 +12,10 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('post')" :active="request()->routeIs('post')">
+                    <x-nav-link :href="route('post')" :active="request()->routeIs('post')" wire:navigate>
                         {{ __('Posts') }}
                     </x-nav-link>
                 </div>
